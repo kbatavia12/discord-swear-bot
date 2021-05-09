b6fde2e546ccb7df0492b44d96bf8a4f96a951d2
Karan Batavia <42770054+kbatavia12@users.noreply.github.com>
2021-05-09 17:20:37 +0530

    First commit

---
+++ bot.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const dotenv = require('dotenv').config();
+const words = require('./words.json')
+const BOT_TOKEN = process.env.BOT_TOKEN
+
+
+const bot = new Discord.Client()
+bot.login(BOT_TOKEN);
+
+
+const randomWord = () => {
+    const randomIndex = Math.floor(Math.random() * words.length);
+
+    return words[randomIndex];
+}
+
+bot.on('ready', evt =>{
+    console.log(`Logged in as ${bot.user.tag}`)
+});
+
+
+bot.on('message', message => {
+    if (message.content.startsWith('#swear ')) {
+        message.reply(randomWord());
+    }
+})
