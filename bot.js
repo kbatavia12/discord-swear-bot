251a952dbf9919f55a6c9a3060a5252a280178df
Karan Batavia <42770054+kbatavia12@users.noreply.github.com>
2021-05-12 20:15:33 +0530

    test changes

--- bot.js
+++ bot.js
@@ -7,20 +7,19 @@ const BOT_TOKEN = process.env.BOT_TOKEN
 const bot = new Discord.Client()
 bot.login(BOT_TOKEN);
 
-
 const randomWord = () => {
     const randomIndex = Math.floor(Math.random() * words.length);
 
     return words[randomIndex];
 }
 
-bot.on('ready', evt =>{
+bot.on('ready', evt => {
     console.log(`Logged in as ${bot.user.tag}`)
 });
 
 
 bot.on('message', message => {
     if (message.content.startsWith('#swear ')) {
-        message.reply(randomWord());
+        console.log(message.content);
     }
 })
